4726031196a0d1e78aa4cde38f4885c2bbf28732
anuj-bansal <anujb@hotmail.com>
2025-09-28 17:56:25 -0700

    build error

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,5 @@
 import { defineConfig } from 'vite';
-import viteImagemin from 'vite-plugin-imagemin';
 
 export default defineConfig({
-  plugins: [
-    viteImagemin({
-      webp: {
-        quality: 80,
-      },
-      pngquant: {
-        quality: [0.7, 0.9],
-      },
-      mozjpeg: {
-        quality: 80,
-      },
-    }),
-  ],
+  plugins: [],
 });
